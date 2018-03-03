8a02e7bc9673d5ffe12c06d257d3456e3f4d9c0a
LiangChaokai <youchoudeyu@qq.com>
2018-03-03 20:36:51 +0800

    Mongo优化

--- backend/lib/mongooseConnect.js
+++ backend/lib/mongooseConnect.js
@@ -5,7 +5,13 @@ var mongoose = require('mongoose'),
     DB_CONN = 'mongodb://localhost:27017/',
     DB_Name = 'ncee';
 var options = {
-    'useMongoClient': true
+    useMongoClient: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
 };
 var DB_DatabasePath = DB_CONN + DB_Name;
 mongoose.Promise = global.Promise;//为了解决过期的问题
